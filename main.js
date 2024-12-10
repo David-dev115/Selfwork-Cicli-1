6e33ad44e712fdcb82dc98c3d84a877d0304f608
David Paparo <d.paparo@tiscali.it>
2024-12-10 18:06:01 +0100

    carico esercizio corretto

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 
+// <!-- Correggo dopo aver visto video/soluzione -->
 
 let num = 2;
 
@@ -7,12 +8,9 @@ let n = 10;
 
 for ( let i = 0; i <= n; i++){
 
-num = i * 2;
+    let prodotto = i*num
 
-
-console.log( ` 2 x ${i} fa ${num}` );
-
-
+console.log( `${num} x  ${i} fa  ${prodotto} `);
 
 }
 
